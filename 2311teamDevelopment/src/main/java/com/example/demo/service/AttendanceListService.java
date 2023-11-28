f3328b0f0b67dee40cf0b9dc3ec4586475069edc
aiko10433 <138350360+aiko10433@users.noreply.github.com>
2023-11-28 23:58:43 +0900

    Revert "11/28"
    This reverts commit d4e3929291835ecb1e79ca39d96ca0db2c64af83.

--- 2311teamDevelopment/src/main/java/com/example/demo/service/AttendanceListService.java
+++ 2311teamDevelopment/src/main/java/com/example/demo/service/AttendanceListService.java
@@ -6,25 +6,51 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.dto.AttendanceListUpdateRequest;
 import com.example.demo.entity.AttendanceListEntity;
 import com.example.demo.repository.AttendanceListRepository;
 
 /**
- * 勤怠情報 Service
+ * ユーザー情報 Service
  */
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class AttendanceListService {
 	/**
-	 * 勤怠情報 Repository
+	 * ユーザー情報 Repository
 	 */
+	//2行追加
 	@Autowired
 	private AttendanceListRepository attendanceRepository;
 	/**
-	 * 勤怠情報 全検索
+	 * ユーザー情報 全検索
 	 * @return  検索結果
 	 */
 	public List<AttendanceListEntity> searchAll() {
 		return attendanceRepository.findAll();
+	}
+	/**
+	 * ユーザー情報 主キー検索
+	 * @return  検索結果
+	 */
+	public AttendanceListEntity findById(Long attendance_id) {
+		return attendanceRepository.findById(attendance_id).get();
+	}
+	/**
+	 * ユーザー情報 更新
+	 * @param  user ユーザー情報
+	 */
+	public void update(AttendanceListUpdateRequest attendanceUpdateRequest) {
+		AttendanceListEntity attendance = findById(attendanceUpdateRequest.getAttendance_id());
+//		attendance.setUser_id(attendanceUpdateRequest.getUser_id());
+//		attendance.setStatus(attendanceUpdateRequest.getStatus());
+//		attendance.setStart_date(attendanceUpdateRequest.getStart_date());
+//		attendance.setStart_time(attendanceUpdateRequest.getStart_time());
+//		attendance.setLeaving_date(attendanceUpdateRequest.getLeaving_date());
+//		attendance.setLeaving_time(attendanceUpdateRequest.getLeaving_time());
+//		attendance.setWorking_time(attendanceUpdateRequest.getWorking_time());
+//		attendance.setWorking_time(attendanceUpdateRequest.getWorking_time());
+//		attendance.setComments(attendanceUpdateRequest.getComments());
+	    attendanceRepository.save(attendance);
 	}
 }
