fbf6f9e2263b9f40cd0c62e3e9a02ea0289e3b89
ChihiroAida <142207215+ChihiroAida@users.noreply.github.com>
2023-12-04 03:48:30 +0900

    Create costListEntity.java

---
+++ 2311teamDevelopment/src/main/java/com/example/demo/entity/costListEntity.java
@@ -0,0 +1,51 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "tb_cost")
+public class costListEntity {   
+	  /**
+	   * 経費ID
+	   */
+	  @Id
+	  @Column(name = "cost_id")
+	  @GeneratedValue(strategy = GenerationType.IDENTITY)
+	  private Integer cost_id;
+	  /**
+	   * ユーザーID
+	   */
+	  @Column(name = "user_id")
+	  private Integer user_id;
+	  /**
+	   * 申請日
+	   */
+	  @Column(name = "application_day")
+	  private Date application_day;
+	  /**
+	   * 項目
+	   */
+	  @Column(name = "category")
+	  private String category;
+	  /**
+	   * コスト
+	   */
+	  @Column(name = "cost")
+	  private Integer cost;
+	  /**
+	   * 備考
+	   */
+	  @Column(name = "comments")
+	  private String comments;
+
+}
