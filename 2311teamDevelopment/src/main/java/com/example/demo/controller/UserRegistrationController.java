ec58d0c090c7882cda6e0bd4b597912540235271
HonokaShibuya <honoka2800@gmail.com>
2023-12-04 00:28:54 +0900

    ユーザ登録

--- 2311teamDevelopment/src/main/java/com/example/demo/controller/UserRegistrationController.java
+++ 2311teamDevelopment/src/main/java/com/example/demo/controller/UserRegistrationController.java
@@ -1,12 +1,21 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.demo.dto.UserEditDeleteRequest;
 import com.example.demo.entity.UserRegistrationEntity;
 import com.example.demo.service.UserRegistrationService;
 
@@ -15,12 +24,40 @@ public class UserRegistrationController {
  @Autowired
  UserRegistrationService userRegistrationService;
 
- @GetMapping
- String list(Model model) {
-  List<UserRegistrationEntity> users = userRegistrationService.searchAll();
-  model.addAttribute("users", users);
-  return "list";
+ @GetMapping(value = "/user/list")
+ public String displaylist(Model model) {
+  List<UserRegistrationEntity> userlist = userRegistrationService.searchAll();
+  model.addAttribute("userlist", userlist);
+  return "user/list";
  }
 
+ @GetMapping(value = "/user/add")
+ public String displayAdd(Model model) {
+	 return "user/add";
+ }
+
+ @GetMapping("/user/{id}")
+public String displayView(@PathVariable Long id, Model model) {
+	 UserRegistrationEntity userRegistrationEntity = userRegistrationService.findByld(id);
+	 model.addAttribute("userData", userRegistrationEntity);
+	 return "user/view";
+ }
+
+ @RequestMapping(value = "/user/update", method = RequestMethod.POST)
+ public String update(@Validated @ModelAttribute
+		 UserEditDeleteRequest userEditDeleteRequest, BindingResult result, Model model) {
+	 if(result.hasErrors()) {
+		 List<String> errorList = new ArrayList<String>();
+	  for(ObjectError error : result.getAllErrors()) {
+		  errorList.add(error.getDefaultMessage());
+	  }
+	  model.addAttribute("validationError", errorList);
+	  return "user/edit";
+	 }
+
+	 userRegistrationService.update(userEditDeleteRequest);
+	 return String format("redirect:/user/%d",
+	userEditDeleteRequest.getId());
+ }
 
 }
