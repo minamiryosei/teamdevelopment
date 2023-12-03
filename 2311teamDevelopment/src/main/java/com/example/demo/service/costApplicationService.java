595c44adb14370c9e68a122d4e2b17edfd2d7e32
ChihiroAida <142207215+ChihiroAida@users.noreply.github.com>
2023-12-03 23:10:31 +0900

    costApplicationService.java

---
+++ 2311teamDevelopment/src/main/java/com/example/demo/service/costApplicationService.java
@@ -0,0 +1,34 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.dto.costApplicationRequest;
+import com.example.demo.entity.costApplicationEntity;
+import com.example.demo.repository.costApplicationRepository;
+
+@Service
+@Transactional (rollbackFor = Exception.class)
+public class costApplicationService {
+	
+@Autowired
+private costApplicationRepository costapplicationRepository;
+
+public List<costApplicationEntity> searchAll() {
+	return costapplicationRepository.findAll();
+}
+
+public void create(costApplicationRequest costapplicationRequest) {
+	costApplicationEntity costapplication = new costApplicationEntity();
+	costapplication.setUser_id(costapplicationRequest.getUser_id());
+	costapplication.setApplication_day(costapplicationRequest.getApplication_day());
+	costapplication.setCategory(costapplicationRequest.getCategory());
+	costapplication.setCost(costapplicationRequest.getCost());
+	costapplication.setComments(costapplicationRequest.getComments());
+	costapplicationRepository.save(costapplication);
+ 
+  }
+}
