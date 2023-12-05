1ecdb2f30a80165006678972172599eb9a7f3fc2
aiko10433 <138350360+aiko10433@users.noreply.github.com>
2023-12-05 11:19:13 +0900

    12/5

---
+++ 2311teamDevelopment/src/main/java/com/example/demo/controller/AttendanceCorrectController.java
@@ -0,0 +1,74 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.dto.AttendanceCorrectUpdateRequest;
+import com.example.demo.entity.AttendanceListEntity;
+import com.example.demo.service.AttendanceCorrectService;
+	/**
+	 * 勤怠修正 Controller
+	 */
+	@Controller
+public class AttendanceCorrectController {
+		/**
+		 * ユーザー情報 Service
+		 */
+		@Autowired
+		private AttendanceCorrectService attendanceService;
+		/**
+		   * ユーザー編集画面を表示
+		   * @param  id 表示するユーザーID
+		   * @param  model Model
+		   * @return  ユーザー編集画面
+		   */
+		  @GetMapping("/templates/{id}/attendanceCorrect")
+		  public String displayEdit(@PathVariable  Long attendance_id, Model model) {
+			AttendanceListEntity attendance = attendanceService.findById(attendance_id);
+			AttendanceCorrectUpdateRequest attendanceUpdateRequest = new AttendanceCorrectUpdateRequest();
+			attendanceUpdateRequest.setAttendance_id(attendance.getAttendance_id());
+			attendanceUpdateRequest.setUser_id(attendance.getUser_id());
+			attendanceUpdateRequest.setStatus(attendance.getStatus());
+			attendanceUpdateRequest.setStart_date(attendance.getStart_date());
+			attendanceUpdateRequest.setStart_time(attendance.getStart_time());
+			attendanceUpdateRequest.setLeaving_date(attendance.getLeaving_date());
+			attendanceUpdateRequest.setLeaving_time(attendance.getLeaving_time());
+			attendanceUpdateRequest.setWorking_time(attendance.getWorking_time());
+			attendanceUpdateRequest.setBreak_time1(attendance.getBreak_time1());
+			attendanceUpdateRequest.setBreak_time2(attendance.getBreak_time2());
+			attendanceUpdateRequest.setComments(attendance.getComments());
+		    model.addAttribute("attendanceUpdateRequest", attendanceUpdateRequest);
+		    return "templates/attendanceCorrect";
+		  }
+		  /**
+		   * ユーザー更新
+		   * @param  userRequest リクエストデータ
+		   * @param  model Model
+		   * @return  ユーザー情報詳細画面
+		   */
+		  @RequestMapping("/templates/attendanceCorrect")
+		  public String update(@Validated  @ModelAttribute  AttendanceCorrectUpdateRequest attendanceUpdateRequest, BindingResult result, Model model) {
+		    if (result.hasErrors()) {
+		      List<String> errorList = new ArrayList<String>();
+		      for (ObjectError error : result.getAllErrors()) {
+		        errorList.add(error.getDefaultMessage());
+		      }
+		      model.addAttribute("validationError", errorList);
+		      return "templates/attendanceCorrect";
+		    }
+		    // ユーザー情報の更新)
+		    attendanceService.update(attendanceUpdateRequest);
+		    return String.format("redirect:/attendanceList/%d", attendanceUpdateRequest.getAttendance_id());
+		  }
+}
---
+++ 2311teamDevelopment/src/main/java/com/example/demo/service/AttendanceCorrectService.java
@@ -0,0 +1,45 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.dto.AttendanceCorrectUpdateRequest;
+import com.example.demo.entity.AttendanceListEntity;
+import com.example.demo.repository.AttendanceListRepository;
+/**
+ * 勤怠情報 Service
+ */
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class AttendanceCorrectService {
+	/**
+	 * 勤怠情報 Repository
+	 */
+	@Autowired
+	private AttendanceListRepository attendanceRepository;
+	/**
+	 * 勤怠情報 主キー検索
+	 * @return  検索結果
+	 */
+	public AttendanceListEntity findById(Long attendance_id) {
+		return attendanceRepository.findById(attendance_id).get();
+	}
+	/**
+	 * 勤怠情報 更新
+	 * @param   勤怠情報
+	 */
+	public void update(AttendanceCorrectUpdateRequest attendanceUpdateRequest) {
+		AttendanceListEntity attendance = findById(attendanceUpdateRequest.getAttendance_id());
+		attendance.setUser_id(attendanceUpdateRequest.getUser_id());
+		attendance.setStatus(attendanceUpdateRequest.getStatus());
+		attendance.setStart_date(attendanceUpdateRequest.getStart_date());
+		attendance.setStart_time(attendanceUpdateRequest.getStart_time());
+		attendance.setLeaving_date(attendanceUpdateRequest.getLeaving_date());
+		attendance.setLeaving_time(attendanceUpdateRequest.getLeaving_time());
+		attendance.setWorking_time(attendanceUpdateRequest.getWorking_time());
+		attendance.setWorking_time(attendanceUpdateRequest.getWorking_time());
+		attendance.setComments(attendanceUpdateRequest.getComments());
+		attendanceRepository.save(attendance);
+	}
+}
