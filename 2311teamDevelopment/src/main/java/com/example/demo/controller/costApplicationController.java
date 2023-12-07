15a9a034223e43edf0591cdfc5327498f88b1529
ChihiroAida <142207215+ChihiroAida@users.noreply.github.com>
2023-12-08 00:31:06 +0900

    Update costApplicationController.java

--- 2311teamDevelopment/src/main/java/com/example/demo/controller/costApplicationController.java
+++ 2311teamDevelopment/src/main/java/com/example/demo/controller/costApplicationController.java
@@ -11,48 +11,64 @@ import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.dto.costApplicationRequest;
+import com.example.demo.entity.costApplicationEntity;
 import com.example.demo.service.costApplicationService;
 
 @Controller
 public class costApplicationController {
-	
-@Autowired
-private costApplicationService costapplicationService;
 
-/**
- * 経費申請を表示
- * @param model Model
- * @return 経費申請画面
- */
-@GetMapping(value ="/")
-public String displayList(Model model) {
-	 model.addAttribute("userRequest", new costApplicationRequest());
-      return "costApplication";
- }
+	@Autowired
+	private costApplicationService costapplicationService;
 
-/**
- * 経費登録
- * @param userRequest リクエストデータ
- * @param model Model
- * @return 経費申請画面
- */
-@GetMapping("/create")
-public String create(@Validated @ModelAttribute costApplicationRequest costapplicationRequest, BindingResult result, Model model) {
-  if (result.hasErrors()) {
-	  
-    //入力チェックエラーの場合
-	  
-    List<String> errorList = new ArrayList<String>();
-    for (ObjectError error : result.getAllErrors()) {
-      errorList.add(error.getDefaultMessage());
-    }
-    model.addAttribute("validationError", errorList);
-    return "costApplication";
-  }
-  // ユーザー情報の登録
-  costapplicationService.create(costapplicationRequest);
-  return "redirect:/costApplication";
-  }
+	/**
+	 * マイページを表示
+	 * @param model Model
+	 * @return マイページ
+	 */
+	@GetMapping(value = "/myPage")
+	public String displayList(Model model) {
+		List<costApplicationEntity> mypage = costapplicationService.searchAll();
+		model.addAttribute("mypage", mypage);
+		return "/myPage";
+	}
+
+	/**
+	 * 経費申請を表示
+	 * @param model Model
+	 * @return 経費申請画面
+	 */
+	@GetMapping("/costapplication")
+	public String displayAdd(Model model) {
+		model.addAttribute("costapplicationRequest", new costApplicationRequest());
+		return "costApplication";
+	}
+
+	/**
+	 * 経費登録
+	 * @param userRequest リクエストデータ
+	 * @param model Model
+	 * @return 経費申請画面
+	 */
+	@PostMapping("/costapplication/add")
+	public String create(@Validated @ModelAttribute costApplicationRequest costapplicationRequest, BindingResult result, Model model) {
+
+		if (result.hasErrors()) {
+
+			//入力チェックエラーの場合
+
+			List<String> errorList = new ArrayList<String>();
+			for (ObjectError error : result.getAllErrors()) {
+				errorList.add(error.getDefaultMessage());
+			}
+			model.addAttribute("validationError", errorList);
+			return "costApplication";
+		}
+
+		// 経費の申請
+		costapplicationService.create(costapplicationRequest);
+		return "redirect:/costApplication";
+	}
 }
