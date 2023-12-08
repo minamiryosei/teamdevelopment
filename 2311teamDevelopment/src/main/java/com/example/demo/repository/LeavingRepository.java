67bb3511fd0f60657c3774bcb09bdf8b545888f3
Matsui-Aimi <137156712+Matsui-Aimi@users.noreply.github.com>
2023-12-09 03:00:27 +0900

    1209attendance

---
+++ 2311teamDevelopment/src/main/java/com/example/demo/repository/LeavingRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.AttendanceEntity;
+
+/**
+ * ユーザー情報 Repository
+ */
+public interface LeavingRepository extends JpaRepository<AttendanceEntity, Integer> {}
---
+++ 2311teamDevelopment/src/main/java/com/example/demo/service/LeavingService.java
@@ -0,0 +1,31 @@
+package com.example.demo.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.dto.LeavingRequest;
+import com.example.demo.entity.LeavingEntity;
+import com.example.demo.repository.LeavingRepository;
+
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class LeavingService {
+	@Autowired
+	private LeavingRepository leavingRepository;
+
+//退勤新規登録
+public void create(LeavingRequest leavingRequest) {
+	LeavingEntity attendance = new LeavingEntity();
+	attendance.setUserId(leavingRequest.getUserId());
+	attendance.setStatus(leavingRequest.getStatus());
+	attendance.setLervingDate(leavingRequest.getLeavingDate());
+	attendance.setLeavingTime(leavingRequest.getLeavingTime());
+	attendance.setBreakTime(leavingRequest.getBreakTime());	
+	attendance.setBreakTimes(leavingRequest.getBreakTimes());	
+	
+
+//	leavingRepository.save(attendance);
+}
+}
+
