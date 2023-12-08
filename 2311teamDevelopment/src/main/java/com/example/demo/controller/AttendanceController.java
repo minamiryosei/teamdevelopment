9b49a12e8e0917b364139c5ce64a46110a0db7e1
Matsui-Aimi <137156712+Matsui-Aimi@users.noreply.github.com>
2023-12-08 23:59:21 +0900

    Update AttendanceController.java

--- 2311teamDevelopment/src/main/java/com/example/demo/controller/AttendanceController.java
+++ 2311teamDevelopment/src/main/java/com/example/demo/controller/AttendanceController.java
@@ -28,18 +28,15 @@ public class AttendanceController {
 	@Autowired
 	private AttendanceService attendanceService;
 
-		public String displayList(Model model) {
-			model.addAttribute("attendanceRequest", new AttendanceRequest());
-			return "attendance";
-	  }
-		 @GetMapping(value = "templates/attendance")
+		
+		 @GetMapping(value = "templates/attendance{@attendance_id}")
 		  public String displayAdd(Model model) {
 		    model.addAttribute("userRequest", new AttendanceRequest());
 		    return "attendance";
 		  }
 			
 		//出勤登録
-	@PostMapping("/user/create")
+	@PostMapping("/create")
 	public String create(@Validated @ModelAttribute AttendanceRequest attendanceRequest, BindingResult result,
 			Model model) {
 		//入力判定
@@ -51,7 +48,8 @@ public class AttendanceController {
 			}
 			//エラー判定後の画面遷移
 			model.addAttribute("validationError", errorList);
-			model.addAttribute("attendanceRequest", attendanceRequest);
+			model.addAttribute("attendanceReque"
+					+ "st", attendanceRequest);
 			return "attendance";
 		}
 		// 出勤情報の登録
