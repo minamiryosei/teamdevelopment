121025552a9492943eb468e0cf752e25092c4c51
ChihiroAida <142207215+ChihiroAida@users.noreply.github.com>
2023-12-13 23:12:20 +0900

    Update costApplicationService.java

--- 2311teamDevelopment/src/main/java/com/example/demo/service/costApplicationService.java
+++ 2311teamDevelopment/src/main/java/com/example/demo/service/costApplicationService.java
@@ -15,34 +15,29 @@ import com.example.demo.repository.costApplicationRepository;
 @Service
 @Transactional (rollbackFor = Exception.class)
 public class costApplicationService {
-	
-@Autowired
-private costApplicationRepository costapplicationRepository;
+
+	@Autowired
+	private costApplicationRepository costapplicationRepository;
 
 
-public List<costApplicationEntity> searchAll() {
-	return costapplicationRepository.findAll();
+	public List<costApplicationEntity> searchAll() {
+		return costapplicationRepository.findAll();
+	}
+
+
+	public void create(costApplicationRequest costapplicationRequest) {
+		costApplicationEntity costapplication = new costApplicationEntity();
+
+		String str = costapplicationRequest.getApplication_day();
+		LocalDate localdate = LocalDate.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		costapplication.setApplication_day(localdate);
+
+		costapplication.setUser_id(costapplicationRequest.getUser_id());
+		costapplication.setCategory(costapplicationRequest.getCategory());
+		costapplication.setCost(costapplicationRequest.getCost());
+		costapplication.setComments(costapplicationRequest.getComments());
+		costapplicationRepository.save(costapplication);
+
+	} 
 }
-
-
-public void create(costApplicationRequest costapplicationRequest) {
-	costApplicationEntity costapplication = new costApplicationEntity();
-	
-	 String str = costapplicationRequest.getApplication_day();
-	  LocalDate localdate = LocalDate.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-	  costapplication.setApplication_day(localdate);
-	 
-	costapplication.setUser_id(costapplicationRequest.getUser_id());
-	costapplication.setCategory(costapplicationRequest.getCategory());
-	costapplication.setCost(costapplicationRequest.getCost());
-	costapplication.setComments(costapplicationRequest.getComments());
-	costapplicationRepository.save(costapplication);
-	
-    } 
-  }
-
-
-
-
-
 
