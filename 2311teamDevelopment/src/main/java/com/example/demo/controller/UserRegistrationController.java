b783bb31937101aeb4356b4ad0d7c92d1824a274
HonokaShibuya <honoka2800@gmail.com>
2023-12-15 23:54:31 +0900

    ユーザ登録

--- 2311teamDevelopment/src/main/java/com/example/demo/controller/UserRegistrationController.java
+++ 2311teamDevelopment/src/main/java/com/example/demo/controller/UserRegistrationController.java
@@ -12,9 +12,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.example.demo.dto.UserRegistrationRequest;
 import com.example.demo.service.UserRegistrationService;
 
