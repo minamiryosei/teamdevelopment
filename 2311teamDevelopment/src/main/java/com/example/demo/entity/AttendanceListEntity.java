50053961485e6b5a1bb9c6d43632f6005a899a52
yutawatanabe0712 <yutaoneok0712@icloud.com>
2023-12-19 23:16:17 +0900

    勤怠一覧マージ

--- 2311teamDevelopment/src/main/java/com/example/demo/entity/AttendanceListEntity.java
+++ 2311teamDevelopment/src/main/java/com/example/demo/entity/AttendanceListEntity.java
@@ -1,8 +1,8 @@
 
 package com.example.demo.entity;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.sql.Time;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -14,7 +14,7 @@ import javax.persistence.Table;
 import lombok.Data;
 
 /**
- * ユーザー情報 Entity
+ * 勤怠情報 Entity
  */
 @Data
 @Entity
@@ -42,37 +42,37 @@ public class AttendanceListEntity  {
 	 * 出勤日
 	 */
 	@Column(name ="start_date")
-	private LocalDate start_date;
+	private Date start_date;
 	/**
 	 * 出勤時間
 	 */
 	@Column(name = "start_time")
-	private LocalTime start_time;
+	private Time start_time;
 	/**
 	 * 退勤日
 	 */
 	@Column(name = "leaving_date")
-	private LocalDate leaving_date;
+	private Date leaving_date;
 	/**
 	 * 退勤時間
 	 */
 	@Column(name = "leaving_time")
-	private LocalTime leaving_time;
+	private Time leaving_time;
 	/**
 	 * 稼働時間
 	 */
 	@Column(name = "working_time")
-	private LocalTime working_time;
+	private Time working_time;
 	/**
 	 * 休憩時間１
 	 */
 	@Column(name = "break_time1")
-	private LocalTime break_time1;
+	private Time break_time1;
 	/**
 	 * 休憩時間２
 	 */
 	@Column(name = "break_time2")
-	private LocalTime break_time2;
+	private Time break_time2;
 	/**
 	 * 修正理由
 	 */
