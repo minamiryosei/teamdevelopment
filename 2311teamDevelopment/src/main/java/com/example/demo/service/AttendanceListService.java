4ea55b05f246fb6887ca2a4c821520354eff0764
aiko10433 <138350360+aiko10433@users.noreply.github.com>
2023-12-20 22:37:24 +0900

    12/20

--- 2311teamDevelopment/src/main/java/com/example/demo/service/AttendanceListService.java
+++ 2311teamDevelopment/src/main/java/com/example/demo/service/AttendanceListService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.demo.dto.AttendanceListUpdateRequest;
 import com.example.demo.entity.AttendanceListEntity;
 import com.example.demo.repository.AttendanceListRepository;
 
@@ -28,29 +27,5 @@ public class AttendanceListService {
 	 */
 	public List<AttendanceListEntity> searchAll() {
 		return attendanceRepository.findAll();
-	}
-	/**
-	 * ユーザー情報 主キー検索
-	 * @return  検索結果
-	 */
-	public AttendanceListEntity findById(Long attendance_id) {
-		return attendanceRepository.findById(attendance_id).get();
-	}
-	/**
-	 * ユーザー情報 更新
-	 * @param  user ユーザー情報
-	 */
-	public void update(AttendanceListUpdateRequest attendanceUpdateRequest) {
-		AttendanceListEntity attendance = findById(attendanceUpdateRequest.getAttendance_id());
-//		attendance.setUser_id(attendanceUpdateRequest.getUser_id());
-//		attendance.setStatus(attendanceUpdateRequest.getStatus());
-//		attendance.setStart_date(attendanceUpdateRequest.getStart_date());
-//		attendance.setStart_time(attendanceUpdateRequest.getStart_time());
-//		attendance.setLeaving_date(attendanceUpdateRequest.getLeaving_date());
-//		attendance.setLeaving_time(attendanceUpdateRequest.getLeaving_time());
-//		attendance.setWorking_time(attendanceUpdateRequest.getWorking_time());
-//		attendance.setWorking_time(attendanceUpdateRequest.getWorking_time());
-//		attendance.setComments(attendanceUpdateRequest.getComments());
-	    attendanceRepository.save(attendance);
 	}
 }
