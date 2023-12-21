de3bccec0a8c05f7f2d3ec76bd152c4b9a33b4be
HonokaShibuya <honoka2800@gmail.com>
2023-12-21 11:57:28 +0900

    ユーザ編集・削除

--- 2311teamDevelopment/src/main/java/com/example/demo/dto/UserEditDeleteRequest.java
+++ 2311teamDevelopment/src/main/java/com/example/demo/dto/UserEditDeleteRequest.java
@@ -2,13 +2,7 @@ package com.example.demo.dto;
 
 import java.io.Serializable;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-
-import org.hibernate.validator.constraints.Length;
-
+import javax.validation.constraints.NotEmpty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -18,23 +12,18 @@ import lombok.EqualsAndHashCode;
 public class UserEditDeleteRequest implements Serializable {
 
 
-	@NotNull(message="名前を入力してください")
-	@Size(max=100, message="名前は100文字以内で入力してください")
+	@NotEmpty(message="名前を入力してください")
 	private String name;
 
-	@NotNull(message="フリガナを入力してください")
-	@Size(max=100, message="フリガナは100文字以内で入力してください")
+	@NotEmpty(message="フリガナを入力してください")
 	private String kana;
 
-	@NotNull(message="メールアドレスを入力してください")
-	@Email(message="メールアドレスの形式で入力してください")
+	@NotEmpty(message="メールアドレスを入力してください")
 	private String email;
 
-	@NotNull(message="パスワードを入力してください")
-	@Length(min= 8, message="パスワードは8桁以上で入力してください")
-	@Pattern(regexp= "^[a-zA-Z0-9]+$", message="パスワードは半角英数字で入力してください")
+	@NotEmpty(message="パスワードを入力してください")
 	private String password;
 
-	@NotNull
+	@NotEmpty
 	private Integer user_id;
 }
